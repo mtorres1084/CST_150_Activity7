13cfa0e0e8b764d53277d8ee94bb0e26c90fc7a7
Miguel Torres Perez <silverwolf1084@outlook.com>
2021-11-01 20:38:11 -0400

    Add project files.

---
+++ CST_150_Activity7/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace CST_150_Activity7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.termsLabel = new System.Windows.Forms.Label();
+            this.termsTB = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.approxLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // termsLabel
+            // 
+            this.termsLabel.AutoSize = true;
+            this.termsLabel.Location = new System.Drawing.Point(12, 9);
+            this.termsLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.termsLabel.Name = "termsLabel";
+            this.termsLabel.Size = new System.Drawing.Size(267, 25);
+            this.termsLabel.TabIndex = 0;
+            this.termsLabel.Text = "Enter the number of terms:";
+            // 
+            // termsTB
+            // 
+            this.termsTB.Location = new System.Drawing.Point(314, 9);
+            this.termsTB.Name = "termsTB";
+            this.termsTB.Size = new System.Drawing.Size(100, 30);
+            this.termsTB.TabIndex = 1;
+            this.termsTB.TextChanged += new System.EventHandler(this.termsTB_TextChanged);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(17, 55);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(115, 64);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "&Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(17, 138);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(115, 62);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "&Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // approxLabel
+            // 
+            this.approxLabel.AutoSize = true;
+            this.approxLabel.Location = new System.Drawing.Point(12, 269);
+            this.approxLabel.Name = "approxLabel";
+            this.approxLabel.Size = new System.Drawing.Size(0, 25);
+            this.approxLabel.TabIndex = 4;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 320);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 25);
+            this.resultLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(471, 351);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.approxLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.termsTB);
+            this.Controls.Add(this.termsLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Approximate Value of Pi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label termsLabel;
+        private System.Windows.Forms.TextBox termsTB;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label approxLabel;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ CST_150_Activity7/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Activity7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int counter;
+            double pi = 1;
+            int terms = int.Parse(termsTB.Text);
+            double sum = 0;
+
+            for(counter = 1; counter <= terms; counter++)
+            {
+                if((counter % 2) == 1)
+                {
+                    sum = sum + 1.0 / ((2.0 * (double)counter) - 1.0);
+                }
+                else
+                {
+                    sum = sum - 1.0 / ((2.0 * (double)counter) - 1.0);
+                }
+                pi = 4 * sum;
+            }
+            approxLabel.Text = "Approximate value of pi after " + terms + " terms";
+            resultLabel.Text = pi.ToString();
+        }
+
+        private void termsTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
